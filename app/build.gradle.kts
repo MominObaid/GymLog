0c75247286bd09e5f248b77b47d60b978524a2cc
obaid <obaidurrahmanobaid@gmail.com>
2026-01-29 15:10:38 +0530

    trying to add api call with Moshi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlin {
-        compilerOptions {
+    kotlin{
+        compilerOptions{
             jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
--- app/src/main/java/com/example/gymlog/RetrofitInstance.kt
+++ app/src/main/java/com/example/gymlog/RetrofitInstance.kt
@@ -1,5 +1,7 @@
 package com.example.gymlog
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -9,20 +11,15 @@ object RetrofitInstance {
     private const val BASE_URL = "https://wger.de/api/v2/"
 //
 //    //Create a Moshi instance with a Kotlin adapter
-//    private val moshi = Moshi.Builder()
-//        .add(KotlinJsonAdapterFactory())
-//        .build()
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
 
     val api : ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(ApiService::class.java)
     }
-
-    //Lazily create an implementation of the ApiService.
-//    val api: ApiService by lazy{
-//        retrofit.create(ApiService::class.java)
-//    }
 }
