a8a388d3a4626fcc166841af1197ef982f9fddf4
obaid <obaidurrahmanobaid@gmail.com>
2026-02-12 14:56:28 +0530

    error at object creating with moshi so replaced with json retrofit

--- app/src/main/java/com/example/gymlog/RetrofitInstance.kt
+++ app/src/main/java/com/example/gymlog/RetrofitInstance.kt
@@ -1,24 +1,49 @@
 package com.example.gymlog
 
+import com.google.gson.Gson
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object RetrofitInstance {
-
-    //The base URL of the API
-    private const val BASE_URL = "https://wger.de/api/v2/"
+//object RetrofitInstance {
 //
-//    //Create a Moshi instance with a Kotlin adapter
-    private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
+//    //The base URL of the API
+//    private const val BASE_URL = "https://wger.de/api/v2"
+////"https://wger.de/api/v2/exerciseinfo/?limit=500&language=2"
+////    //Create a Moshi instance with a Kotlin adapter
+//    private val moshi = Moshi.Builder()
+//        .add(KotlinJsonAdapterFactory())
+//        .build()
+//
+//    val api : ApiService by lazy {
+//        Retrofit.Builder()
+//            .baseUrl(BASE_URL)
+//            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .build()
+//            .create(ApiService::class.java)
+//    }
+//}
+
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://wger.de/api/v2"
+
+    private val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(logging)
         .build()
 
-    val api : ApiService by lazy {
+    val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
             .create(ApiService::class.java)
     }
