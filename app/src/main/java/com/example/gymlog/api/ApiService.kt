51c540ff27cdb87b8ae15d39bdd60fa840f45376
obaid <obaidurrahmanobaid@gmail.com>
2026-03-05 20:03:57 +0530

    update

--- app/src/main/java/com/example/gymlog/api/ApiService.kt
+++ app/src/main/java/com/example/gymlog/api/ApiService.kt
@@ -5,10 +5,10 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/exerciseinfo")     //https://wger.de/api/v2/exerciseinfo/?language=2&limit=500
+    @GET("exerciseinfo/")     //https://wger.de/api/v2/exerciseinfo/?language=2&limit=500
     suspend fun getExercises(
-        @Query("&limit") limit : Int = 500,
-        @Query("/?language") language: Int = 2
+        @Query("limit") limit : Int = 500,
+        @Query("language") language: Int = 2
 
     ): Response<ExerciseApiResponse>
 }
