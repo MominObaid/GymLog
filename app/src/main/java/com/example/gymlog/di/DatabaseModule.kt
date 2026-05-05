67fef183f536fd8a900c4f1cc4ee062bf7694c72
obaid <obaidurrahman@gmail.com>
2026-05-05 18:17:17 +0530

    added DI for more structured, testable, modular, maintainable and clean code

---
+++ app/src/main/java/com/example/gymlog/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.example.gymlog.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.gymlog.model.WorkoutDao
+import com.example.gymlog.model.WorkoutDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): WorkoutDatabase {
+        return Room.databaseBuilder(
+            context,
+            WorkoutDatabase::class.java,
+            "workout_database"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideWorkoutDao(database: WorkoutDatabase): WorkoutDao {
+        return database.workoutDao()
+    }
+}
