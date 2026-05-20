90c99e439646bf18f63c2b0407c09f82f4b132a1
obaid <obaidurrahman@gmail.com>
2026-05-21 03:06:17 +0530

    refactor: redesign dashboard into modern home hub architecture

--- app/src/main/java/com/example/gymlog/GymLogApplication.kt
+++ app/src/main/java/com/example/gymlog/GymLogApplication.kt
@@ -1,7 +1,13 @@
 package com.example.gymlog
 
 import android.app.Application
+import com.example.gymlog.utils.NotificationHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GymLogApplication : Application()
+class GymLogApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        NotificationHelper.createNotificationChannels(this)
+    }
+}
