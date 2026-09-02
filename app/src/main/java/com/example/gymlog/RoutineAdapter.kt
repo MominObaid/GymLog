d8a1352173154427505caeeb28c062908997d723
obaid <obaidurrahman@gmail.com>
2026-09-02 18:31:16 +0530

    refactor: redesign dashboard

--- app/src/main/java/com/example/gymlog/RoutineAdapter.kt
+++ app/src/main/java/com/example/gymlog/RoutineAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.gymlog
 
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
